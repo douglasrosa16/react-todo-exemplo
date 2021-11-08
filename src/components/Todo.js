749d198b2f4269a559b1b29aa933f30982369ab6
douglasrosa16 <douglasoliveira163425@gmail.com>
2021-11-08 18:28:14 -0400

    ajustes

---
+++ src/components/Todo.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import { RiCloseCircleLine } from 'react-icons/ri';
+
+const Todo = ({ todos, completeTodo, removeTodo }) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ''
+  }); 
+
+  return todos.map((todo, index) => (
+    <div
+      className={'todo-row'}
+      key={index}
+    >
+      <div key={todo.id}>
+        {todo.text}
+      </div>
+      <div className='icons'>
+        <RiCloseCircleLine
+          onClick={() => removeTodo(todo.id)}
+          className='delete-icon'
+        />      
+      </div>
+    </div>
+  ));
+};
+
+export default Todo;
---
+++ src/components/TodoList.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = todo => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);  
+  };
+
+  const removeTodo = id => {
+    const removedArr = [...todos].filter(todo => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  return (
+    <>
+      <h1>Lista de Itens</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}        
+        removeTodo={removeTodo}        
+      />      
+    </>
+  );
+}
+
+export default TodoList;
